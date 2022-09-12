b0b0df57af47a37703db4f5c1cde4d75435ae3ef
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-11 20:57:14 -0500

    Progress Next

--- script.js
+++ script.js
@@ -37,6 +37,6 @@ function update() {
 
     const actives = document.querySelectorAll('.active')
 
-    console.log(actives.length, circles.length)
+    progress.style.width = (actives.length -1) /(circles.length - 1) * 100 + '%'
 
 }
